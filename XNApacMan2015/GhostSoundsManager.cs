193d94eb823db03eab3a4cd67c5f2ef29960988d
StevePro7 <steveprozune@hotmail.com>
2019-12-21 16:12:51 +0000

    no message

---
+++ XNApacMan2015/GhostSoundsManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+
+namespace XNAPacMan {
+	/// <summary>
+	/// All four ghosts use the same sounds, and only one can be played at a time. So, instead of having to
+	/// synchronize each other, they use this class.
+	/// </summary>
+	static class GhostSoundsManager
+	{
+	}
+
+       
+}
